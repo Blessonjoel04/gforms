ab3f2b9695779f8e8ba46060f8fe7c36ff9f476f
Blessonjoel04 <blessonjoel2004@gmail.com>
2026-02-12 19:31:17 +0530

    Updated public link

--- conf/settings.py
+++ conf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-rcbjvhcb62%jv-x6-)w(h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '16.170.37.216']
 
 
 # Application definition
